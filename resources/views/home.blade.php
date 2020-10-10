bae91475cd41f11cce2fc86bab741d3d10e4c401
Alexis Navarro <37815457+ICKillerGH@users.noreply.github.com>
2020-10-10 14:42:00 -0400

    assets version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hechicera hindu</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@600;700&family=Nunito&family=Shrikhand&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -392,6 +392,6 @@
         </section>
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,3 +19,7 @@ mix.js('resources/js/app.js', 'public/js')
     postCss: [ tailwindcss('./tailwind.config.js') ],
   })
   .browserSync('hechi.localhost');
+
+if (mix.inProduction()) {
+    mix.version();
+}
