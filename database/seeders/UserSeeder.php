36f6095dd0dd5b5f7e0a84dbc59d245651e7e25c
Alexis Navarro <37815457+ICKillerGH@users.noreply.github.com>
2020-10-15 10:40:47 -0400

    nueva contrasena para el admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'janicson herrera',
             'email' => 'admin@admin.com',
+            'password' => bcrypt('culoancho'),
         ]);
     }
 }
