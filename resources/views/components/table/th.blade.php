c08d4ea2e4106a82df04a0c5b7b8a2ef44edb5a7
Alexis Navarro <37815457+ICKillerGH@users.noreply.github.com>
2020-10-16 13:34:47 -0400

    ajusted en los grises (100 y 200 estaban invertidos)

--- resources/views/components/table/th.blade.php
+++ resources/views/components/table/th.blade.php
@@ -1,3 +1,3 @@
-<th {{ $attributes->merge(['class' => 'px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-semibold text-gray-600 uppercase tracking-wider']) }}>
+<th {{ $attributes->merge(['class' => 'px-6 py-3 bg-gray-200 text-left text-xs leading-4 font-semibold text-gray-600 uppercase tracking-wider']) }}>
     {{ $slot }}
 </th>
